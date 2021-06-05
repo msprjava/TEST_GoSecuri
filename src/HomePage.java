e98f174e387b5a11ced25c603b4a965605cf40a1
msprjava <82873767+msprjava@users.noreply.github.com>
2021-06-05 14:03:54 +0200

    Update HomePage.java

--- src/HomePage.java
+++ src/HomePage.java
@@ -25,7 +25,7 @@ public class HomePage {
                 "</p><footer><p>Copyright GoSecuri - Tous droits réservés<br></p></footer>" +
                 "</body></html>";
 
-        File accueil = new File("D:\\TEST\\homepage.html");
+        File accueil = new File("/var/www/html/homepage.html");
 
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter(accueil));
