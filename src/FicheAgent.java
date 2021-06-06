c0e9e9cbffe402f9053443f60e3af7e15715a468
msprjava <82873767+msprjava@users.noreply.github.com>
2021-06-06 12:07:35 +0200

    Update FicheAgent.java

--- src/FicheAgent.java
+++ src/FicheAgent.java
@@ -60,7 +60,7 @@ public class FicheAgent {
         System.out.println("<!DOCTYPE html><html><head><meta charset=\"utf-8\">" +
                 "<link rel=\"stylesheet\" href=\"style.css\"/><title>Fiche - " + ligne1 + ligne2 + "</title></head>" +
                 "<header><h1><a href=\"homepage.html\">GoSecuri</a></h1>" +
-                "<div class=\"logo\"><img src=\"logo.jpg\"></div></header>" +
+                "<div class=\"logo\"><a href=\"homepage.html\"><img src=\"logo.jpg\"></a></div></header>" +
                 "<body><h1>" + ligne2 + " " + ligne1 + "</h1><br>" +
                 "<p> <br>" +
                 "<div class=\"photo\"><img src=\"cberthier.jpg\"></div>" +
