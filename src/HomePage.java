c10ab2d7becd13f0f1e45aecf20dae42d13e87b3
msprjava <82873767+msprjava@users.noreply.github.com>
2021-06-06 08:45:12 +0200

    Update HomePage.java

--- src/HomePage.java
+++ src/HomePage.java
@@ -12,7 +12,7 @@ public class HomePage {
                 "<header><h1>GoSecuri</h1></header>" +
                 "<title>Home page - GoSecuri</title></head>" +
                 "<body><h1>Accueil</h1></body><br><br><br>" +
-                "<h2>Fiche agents</h2><br><br>" +
+                "<h2>Fiche agents</h2><br><br><br><br>" +
 
                 "<ul><li><a href=\"CorinneBerthier.html\">Corinne Berthier</a></li><br>" +
                 "<li><a href=\"SamConnor.html\">Sam Connor</a></li><br>" +
